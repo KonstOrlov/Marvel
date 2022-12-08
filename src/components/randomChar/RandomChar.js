76a8e6e8adf872091117dcbd84d906e746942a93
Konstantin Orlov <konstorlovrus@gmail.com>
2022-12-08 23:07:21 +0300

    lifecycles methods

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -20,8 +20,23 @@ class RandomChar extends Component {
 
   marvelService = new MarvelService();
 
+  componentDidMount() {
+    this.updateChar();
+    this.timeId= setTimeout(this.updateChar, 3000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeId)
+  }
+
   onCharLoaded = (char) => {
     this.setState({char, loading: false})
+  }
+
+  onCharLoading = () => {
+    this.setState({
+      loading: true
+    })
   }
 
   onError = () => {
@@ -33,6 +48,7 @@ class RandomChar extends Component {
 
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
@@ -43,7 +59,7 @@ class RandomChar extends Component {
     const {char, loading, error} = this.state;
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? <View char={char}/> : null
+    const content = !(loading || error) ? <View char={char}/> : null;
 
     return (
       <div className="randomchar" >
@@ -58,10 +74,10 @@ class RandomChar extends Component {
           <p className="randomchar__title" >
             Or choose another one
           </p >
-          <button className="button button__main" >
+          <button onClick={this.updateChar} className="button button__main" >
             <div className="inner" >try it</div >
           </button >
-          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
         </div >
       </div >
     )
@@ -70,10 +86,15 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
   const {name, description, thumbnail, homepage, wiki} = char;
+  let imgStyle = {'objectFit' : 'cover'};
+  if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+    imgStyle = {'objectFit' : 'contain'};
+  }
+
 
   return (
     <div className="randomchar__block" >
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
       <div className="randomchar__info" >
         <p className="randomchar__name" >{name}</p >
         <p className="randomchar__descr" >
