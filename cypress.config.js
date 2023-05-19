e6ee5f85339ad197fbb44df6bd0532f00a339987
Konstantin Orlov <konstorlovrus@gmail.com>
2023-05-19 21:38:25 +0700

    connected allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
-  projectId: '2py9p8',
+  projectId: "2py9p8",
   e2e: {
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
     baseUrl: "https://konstorlov.github.io/Marvel/",
     viewportHeight: 960,
     viewportWidth: 1200,
